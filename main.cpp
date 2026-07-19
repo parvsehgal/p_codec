0cda393797bfd74a8b9f05e16923837809ba1c93
parvsehgal <parvsehgal2004@gmail.com>
2026-07-19 22:19:44 +0530

    able to read rgb values

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
-void read_file(string file_path) {
-  ifstream file{file_path, ios::ate | ios::binary};
-  cout << file.tellg() << endl;
-}
+class chroma {
+public:
+  vector<unsigned char> readFile(unsigned int height, unsigned int width,
+                                 string filePath) {
+    ifstream file{filePath, ios::binary};
+    size_t bufferSize = height * width * 3;
+    vector<unsigned char> buffer(bufferSize);
+    file.read(reinterpret_cast<char *>(buffer.data()), bufferSize);
+    cout << file.tellg() << endl;
+    return buffer;
+  }
+  void generateSubsample(unsigned int height, unsigned int width,
+                         string filePath) {
+    vector<unsigned char> buffer = readFile(height, width, filePath);
+    cout << (int)buffer[0] << " " << (int)buffer[1] << " " << (int)buffer[2];
+  }
+};
+
 int main() {
-  string currPath = "raw_images/paul.raw";
-  read_file(currPath);
+  chroma chromaObj;
+  string filePath = "./raw_images/paul.raw";
+  chromaObj.generateSubsample(736, 1124, filePath);
 }
